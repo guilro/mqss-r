6885c8423a50bff50a220002bd32a327bd30f781
Guillaume Royer <guilro@redado.org>
2013-12-15 12:51:31 +0100

    avec variable nb heure travail

--- ACM.R
+++ ACM.R
@@ -1,6 +1,6 @@
 
 # Les variables étant TEO$Variable1, nécessairement de type facteur
-ACM <- subset (TEO, select = c(AidesRecues, AideAdminFamille, EmpruntFamille, GardeFamille, Caution, MoyenEmploi, EntretienConjointPresent, EntretienEnfantsPresents, EntretienParentsPresents, EntretienAutreFamillePresents, PbArgentFamilleAv18Ans, ContactFamilleEtranger, FreqRencontreFamille, Enfants, AppartenanceAsociationPaysOrigine, Pretbancaire, AppartenancAssociationSolidarite, AideVersEtranger, AmisMemeOrigine, AideaPersonneExterieure, NbLogement, AideDevoirAsso))
+ACM <- subset (TEO, select = c(AidesRecues, AideAdminFamille, EmpruntFamille, GardeFamille, Caution, MoyenEmploi, EntretienConjointPresent, EntretienEnfantsPresents, EntretienParentsPresents, EntretienAutreFamillePresents, PbArgentFamilleAv18Ans, ContactFamilleEtranger, FreqRencontreFamille, Enfants, AppartenanceAsociationPaysOrigine, Pretbancaire, AppartenancAssociationSolidarite, AideVersEtranger, AmisMemeOrigine, AideaPersonneExterieure, NbLogement, AideDevoirAsso, NbHeuresTravail))
 
 res.ACM <- MCA (ACM, level.ventil=0.005, graph=F)
 
