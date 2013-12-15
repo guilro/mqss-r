d536476f3e520f3710fdbf48f644892c99357338
Guillaume Royer <guilro@redado.org>
2013-12-15 11:49:09 +0100

    maj nom de variable

--- ACM.R
+++ ACM.R
@@ -5,11 +5,11 @@ ACM <- subset (TEO, select = c(AidesRecues, AideAdminFamille, EmpruntFamille, Ga
 res.ACM <- MCA (ACM, level.ventil=0.005, graph=F)
 
 # Etude des valeurs propres : inertie des axes
-valprop.ACM1 <- res.ACM1$eig[1:10,]
-write.infile(valprop.ACM1, file="/le/dossier/que/vous/voulez/ACM.valprop.csv", sep=";")
-valprop.ACM1
-copie(valprop.ACM1)
-barplot(res.ACM1$eig[1:10,2], main="Histogramme des valeurs propres", names.arg=1:10, xlab="Axes", ylab="Pourcentage d'inertie") 
+valprop.ACM <- res.ACM$eig[1:10,]
+write.infile(valprop.ACM, file="../ACM.valprop.csv", sep=";")
+valprop.ACM
+copie(valprop.ACM)
+barplot(res.ACM$eig[1:10,2], main="Histogramme des valeurs propres", names.arg=1:10, xlab="Axes", ylab="Pourcentage d'inertie") 
 
 # Representation des variables
 plot.MCA(res.ACM, choix="var", axes=c(1,2))
@@ -31,14 +31,14 @@ plot.MCA(res.ACM, invisible=c("ind"), title="Nuage des modalites actives Plan 1-
 plot.MCA(res.ACM, invisible=c("ind"), title="Nuage des modalites actives Plan 1-2", axes=c(1,2), cex=0.8, selectMod=c(1:2)) # On sélectionne certaines modalités
 
 #Par exemple, on sélectionne donc manuellement les modalités que l'on souhaite représenter sur le premier axe
-plot.MCA(res.acm1, invisible=c("ind"), title="Nuage des modalites actives Plan 1-2", axes=c(1,2), autoLab="auto", selectMod=c(1,2,4,10,42,46,50)) #premier axe
+plot.MCA(res.ACM, invisible=c("ind"), title="Nuage des modalites actives Plan 1-2", axes=c(1,2), autoLab="auto", selectMod=c(1,2,4,10,42,46,50)) #premier axe
 
 #On ajoute le deuxième axe, par exemple
-plot.MCA(res.acm1, invisible=c("ind"), title="Nuage des modalites actives Plan 1-2", axes=c(1,2), autoLab="auto",  selectMod=c(1,2,4,10,42,46,50, # pour les modalités significatives sur le premier axe#
+plot.MCA(res.ACM, invisible=c("ind"), title="Nuage des modalites actives Plan 1-2", axes=c(1,2), autoLab="auto",  selectMod=c(1,2,4,10,42,46,50, # pour les modalités significatives sur le premier axe#
 5,8,21,25,41,45,49)) # pour les modalités significatives sur le deuxième axe
 
 # On peut vouloir zoomer sur la partie du graphique la plus intéressante, en rognant sur les marges
-plot.MCA(res.acm1, invisible=c("ind"), title="Nuage des modalites actives Plan 1-2", axes=c(1,2), autoLab="auto",
+plot.MCA(res.ACM, invisible=c("ind"), title="Nuage des modalites actives Plan 1-2", axes=c(1,2), autoLab="auto",
          selectMod=c(1,2,4,10,42,46,50, #premier axe
                      5,8,21,25,41,45,49)) #deuxième axe
 xlim=c(-1,2.5), ylim=c(-1.2,2.1)) #attention à être cohérents avec les coordonnées des points
